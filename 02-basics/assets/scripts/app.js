a75b3f973ab8c05da5f4404141db48d32ae09d84
alexeispirit <alexeispirit@gmail.com>
2019-10-31 10:46:18 +0500

    27-adding-custom-function

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -1,6 +1,13 @@
 const defaltResult = 0;
 let currentResult = defaltResult;
 
+function add(a, b) {
+  const result = a + b;
+  alert(`The result is ${result}`);
+}
+
+add(1, 2);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaltResult}  + 10) * 3 / 2 - 1`;
