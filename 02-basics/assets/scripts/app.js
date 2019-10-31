223b27f69fcb991974d99f315e0693a3585d2d9f
alexeispirit <alexeispirit@gmail.com>
2019-10-31 10:09:08 +0500

    24-using-constants

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -1,7 +1,8 @@
-let currentResult = 0;
+const defaltResult = 0;
+let currentResult = defaltResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(" + currentResult + " + 10) * 3 / 2 - 1";
+let calculationDescription = "(" + defaltResult + " + 10) * 3 / 2 - 1";
 
 outputResult(currentResult, calculationDescription);
