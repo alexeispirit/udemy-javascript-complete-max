5b10385ab7272c09423f050b80e1067f6cfb3892
alexeispirit <alexeispirit@gmail.com>
2019-10-31 10:30:55 +0500

    25-more-on-strings

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentResult = defaltResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(" + defaltResult + " + 10) * 3 / 2 - 1";
+let calculationDescription = `(${defaltResult}  + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
