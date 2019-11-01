2ffabfd937b97ad0aba7ae5bd89d08cce5a8095b
alexeispirit <alexeispirit@gmail.com>
2019-11-01 08:49:22 +0500

    38-splitting-code-into-functions

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaltResult = 0;
 let currentResult = defaltResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult += userInput.value;
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult += enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
