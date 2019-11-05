4bb076ba452c928ee87fc516ac69a8cf42c4535c
alexeispirit <alexeispirit@gmail.com>
2019-11-05 09:56:21 +0500

    100-for-in-loop

--- 02-monster-killer/assets/scripts/app.js
+++ 02-monster-killer/assets/scripts/app.js
@@ -164,8 +164,13 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  for (const item of battleLog) {
-    console.log(item);
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log(`#${i}`);
+    for (const key in logEntry) {
+      console.log(`${key} = ${logEntry[key]}`);
+    }
+    i++;
   }
 }
 
