4a89f161403f793afcd9d2b4d30621c36694aff1
alexeispirit <alexeispirit@gmail.com>
2019-11-13 14:25:23 +0500

    240-getting-started-with-oop-code

---
+++ 06-mini-shop-oop/assets/scripts/app.js
@@ -0,0 +1,42 @@
+const productList = {
+  products: [
+    {
+      title: "Pillow",
+      imageUrl:
+        "https://images.unsplash.com/photo-1559051668-9024c9b5e84b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      price: 19.99,
+      description: "Soft pillow"
+    },
+    {
+      title: "Carpet",
+      imageUrl:
+        "https://images.unsplash.com/photo-1534889156217-d643df14f14a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      price: 89.99,
+      description: "Pretty carpet"
+    }
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div>
+          <img src="${prod.imageUrl}" alt="${prod.title}" />
+          <div class="product-item__content">
+            <h2>${prod.title}</h2>
+            <h3>$${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+};
+
+productList.render();
