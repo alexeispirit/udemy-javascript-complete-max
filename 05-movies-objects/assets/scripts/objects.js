4f42083bf7c7723748e5401264414ae57c91b037
alexeispirit <alexeispirit@gmail.com>
2019-11-13 11:01:41 +0500

    235-setters-and-getters

--- 05-movies-objects/assets/scripts/objects.js
+++ 05-movies-objects/assets/scripts/objects.js
@@ -25,7 +25,7 @@ const renderMovies = (filter = "") => {
     // const { title: movieTitle } = info;
     let text = movie.getFormattedTitle() + " - ";
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== "title" && key !== "_title") {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -39,17 +39,22 @@ const addMovieHandler = () => {
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
 
-  if (
-    title.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
-  ) {
+  if (extraName.trim() === "" || extraValue.trim() === "") {
     return;
   }
 
   const newMovie = {
     info: {
-      title,
+      set title(value) {
+        if (value.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = value;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue
     },
     id: Math.random().toString(),
@@ -57,6 +62,8 @@ const addMovieHandler = () => {
       return this.info.title.toUpperCase();
     }
   };
+
+  newMovie.info.title = title;
 
   movies.push(newMovie);
   renderMovies();
