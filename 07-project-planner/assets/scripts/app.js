9518fee09a0f30caa79febaa2c8c678c19f420ad
alexeispirit <alexeispirit@gmail.com>
2019-11-14 12:43:09 +0500

    281-passing-method-references-around

--- 07-project-planner/assets/scripts/app.js
+++ 07-project-planner/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton();
   }
@@ -12,7 +13,7 @@ class ProjectItem {
   connectSwitchButton() {
     const projectItemElement = document.getElementById(this.id);
     const switchBtn = projectItemElement.querySelector("button:last-of-type");
-    switchBtn.addEventListener("click", null);
+    switchBtn.addEventListener("click", this.updateProjectListsHandler);
   }
 }
 
@@ -20,16 +21,26 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
     console.log(this.projects);
   }
 
-  addProject() {}
+  setSwitchHandlerFunction(SwitchHandlerFunction) {
+    this.switchHandler = SwitchHandlerFunction;
+  }
+
+  addProject() {
+    console.log(this);
+  }
 
   switchProject(projectId) {
+    this.switchHandler(this.projects.find(p => p.id === projectId));
     this.project = this.projects.filter(p => p.id !== projectId);
   }
 }
@@ -38,6 +49,13 @@ class App {
   static init() {
     const activeProjectsList = new ProjectList("active");
     const finishedProjectsList = new ProjectList("finished");
+
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
   }
 }
 
